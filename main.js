aa236eae988f4dc907dbe8b7aa91711db53ab6a4
jinyu <823457467@qq.com>
2021-10-18 13:02:32 +0800

    6

--- main.js
+++ main.js
@@ -5,41 +5,33 @@ function getAllArgs() {
 function getPath(string) {
      let args = getAllArgs();
      if (args.length === 0) {
-          return "please enter node main.js  -i 指定文件夹 -o 输出文件夹";
+          return "please enter node main.js  -i 指定文件夹";
      }
      if (args.length > 0) {
           for (let p = 0; p < args.length; p++) {
                if (args[p].startsWith(string)) {
-                    return args[p + 1]
+                    return args[p + 1];
                }
           }
      }
 }
-// console.log(getAllArgs());
-// console.log(getPath("-i"));
-
+console.log(getAllArgs());
+console.log(getPath("-i"));
 
 const input = require("./input.js");
 const data = require('./data.js');
-//const mkdirsSync = require('./output.js');
 const output = require('./output.js');
-var fs = require("fs");
-res = input(getPath("-i"));
+const fs = require("fs");
 const path = require("path");
-var reg=[];
-res.forEach(function(filename){
-     //console.log(filename);
+res = input(getPath("-i"));
+var reg = [];
+res.forEach(function (filename) {
      reg = JSON.parse(fs.readFileSync(filename));
      reg = reg.reg;
-     //console.log(reg);
      data(reg);
      //console.log(svg);
-     //console.log(path.basename(filename));
-     //path.basename(filename,'.json')获取路径的最后一个文件名（不包括扩展名）
-     //output(`svg文件/${filename}`);
      dirname = `svg文件/${filename}`;
      dirname = path.dirname(dirname);
      result = `${dirname}/${path.basename(filename, '.json')}.svg`;
-    output(dirname,result,svg);
-    //output(`svg文件/${filename}/${path.basename(filename,'.json')}.svg`,svg);
-     });
+     output(svg);
+});
